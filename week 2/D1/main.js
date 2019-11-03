148483601f61df29f82745e613ed51f7da895f4a
MathildeCh <MathildeCh@github.com>
2019-11-04 00:34:15 +0200

    commit week 2

---
+++ week 2/D1/main.js
@@ -0,0 +1,15 @@
+var param = 0;
+var param1 = 0;
+var param2 = 0;
+
+function my_f() {
+  document.InnerHTML = value;
+}
+
+if (param != NaN || param1 == '+' || param2 != Nan) {
+    param + param2;
+  } else (param != NaN || param1 == '-' || param2 != Nan) {
+    param - param2;
+  } else (param != NaN || param1 == '/' || param2 != Nan) {
+    alert(return param / param2);
+  }
